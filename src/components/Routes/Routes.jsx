71a5f0f96847af95c55b8e8ed6c5db4bd446cf31
StepanovMaksim <130572592+StepanovMaksim@users.noreply.github.com>
2023-11-27 20:20:54 +0300

    2

--- src/components/Routes/Routes.jsx
+++ src/components/Routes/Routes.jsx
@@ -10,13 +10,15 @@ import SingleCategory from "../Categories/SingleCategory";
 import Cart from "../Cart/Cart";
 
 const AppRoutes = () => (
-  <Routes>
-    <Route index element={<Home />} />
-    <Route path={ROUTES.PRODUCT} element={<SingleProduct />} />
-    <Route path={ROUTES.PROFILE} element={<Profile />} />
-    <Route path={ROUTES.CATEGORY} element={<SingleCategory />} />
-    <Route path={ROUTES.CART} element={<Cart />} />
-  </Routes>
-);
+	<Routes>
+		<Route index element={<Home />} />
+		<Route path='/Shop-Med' element={<Home />} />
+		<Route path={ROUTES.ROBE} element={<Robe />} />
+		<Route path={ROUTES.PRODUCT} element={<SingleProduct />} />
+		<Route path={ROUTES.PROFILE} element={<Profile />} />
+		<Route path={ROUTES.CATEGORY} element={<SingleCategory />} />
+		<Route path={ROUTES.CART} element={<Cart />} />
+	</Routes>
+)
 
 export default AppRoutes;
